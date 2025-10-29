7d7b7bef72def628ca646742de80448356e95499
chaitrrareddy <163492742+chaitrrareddy@users.noreply.github.com>
2025-10-29 20:44:00 +0530

    Update app.py

--- app.py
+++ app.py
@@ -85,4 +85,4 @@ def history():
     return render_template("history.html", rows=rows)
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
+    app.run(host="0.0.0.0", port='5000',debug=True)
